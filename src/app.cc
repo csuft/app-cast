a77fe5cc9094002fad101001d037edb8c5003356
zhangzhongke <zhangzhongke007@163.com>
2020-12-09 14:42:53 +0800

    chore: update version number

--- src/app.cc
+++ src/app.cc
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
 	mainWindow.show();
 
 	win_sparkle_set_appcast_url("https://raw.githubusercontent.com/csuft/app-cast/master/doc/appcast.xml");
-	win_sparkle_set_app_details(L"insta360.com", L"Sparkle Demo App", L"1.0.0");
+	win_sparkle_set_app_details(L"insta360.com", L"Nova Studio", L"1.0.0");
 	win_sparkle_set_dsa_pub_pem(reinterpret_cast<const char*>(QResource(":/key/dsa_pub.pem").data()));
 
 	int a = app.exec();
