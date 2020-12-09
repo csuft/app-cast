3f1afcb6855c6c4a9f68b1c0e72b84cae6406848
zhangzhongke <zhangzhongke007@163.com>
2020-12-09 10:54:24 +0800

    feat: update appcast files

--- src/app.cc
+++ src/app.cc
@@ -11,12 +11,12 @@ int main(int argc, char* argv[]) {
 	mainWindow.show();
 
 	win_sparkle_set_appcast_url("");
-	win_sparkle_set_app_details(L"insta360.com", L"Sparkle Demo App", L"1.0");
+	win_sparkle_set_app_details(L"insta360.com", L"Sparkle Demo App", L"1.0.0");
 	win_sparkle_set_dsa_pub_pem(reinterpret_cast<const char*>(QResource(":/key/dsa_pub.pem").data()));
 
 	int a = app.exec();
 
-	win_sparkle_cleanup();
+	win_sparkle_cleanup();  
 
 	return a;
 }
